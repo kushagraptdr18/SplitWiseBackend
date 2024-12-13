c50ffed5e9cdd1dd453cfa1180512d78dc55baa1
kushagra patidar <kushagraptdr18@gmail.com>
2024-12-13 11:56:15 +0530

    https correction

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -50,7 +50,7 @@ module.exports.loginPageController = async (req, res) => {
         // Set a cookie named 'token' with the generated JWT
         res.cookie('token', token, {
           httpOnly: true,  // Only accessible via HTTP (not through JavaScript)
-          secure: false,  // Set to true if using HTTPS
+          secure: true,  // Set to true if using HTTPS
           maxAge: 24 * 60 * 60 * 1000  // 1 day in milliseconds
         });
         
