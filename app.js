9bb83c9ec7665f95fcaf3787caf92fab8d29bcb5
kushagra patidar <kushagraptdr18@gmail.com>
2025-05-15 21:37:44 +0530

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 // const cors = require('cors');
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://splitwise-frontend-silk.vercel.app"],
+  origin: true,
   credentials: true
 }));
 
